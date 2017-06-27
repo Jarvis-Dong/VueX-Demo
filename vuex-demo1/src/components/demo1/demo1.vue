adef18e966c43605067dfb38dfb82748b8a12054
Jarvis Dong <jarvis.dong1992@gmail.com>
2017-06-27 16:25:48 +0800

    [VueX]我忘了我写了些啥了。。。

--- vuex-demo1/src/components/demo1/demo1.vue
+++ vuex-demo1/src/components/demo1/demo1.vue
@@ -1,11 +1,34 @@
 <template>
   <div>
     <p>demo1</p>
+
+    <p>{{ count }}</p>
+    <button @click="increment">add</button>
+    <button @click="decrement">minus</button>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-
+  export default {
+    data () {
+      return {}
+    },
+    computed: {
+      count () {
+        return this.$store.state.count
+      }
+    },
+    methods: {
+      increment () {
+        this.$store.commit('increment', {
+          amount: 2
+        })
+      },
+      decrement () {
+        this.$store.commit('decrement')
+      }
+    }
+  }
 </script>
 
 <style lang="stylus">
--- vuex-demo1/src/main.js
+++ vuex-demo1/src/main.js
@@ -3,13 +3,61 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
+import { increment } from './mutation/mutation-types'
+
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+    todos: [
+      {id: 1, text: '...', done: true},
+      {id: 2, text: '...', done: false}
+    ]
+  },
+  /*
+   ***********************
+   类似于function
+   更改 Vuex 的 store 中的状态的唯一方法是提交 mutation
+   唤醒一个 mutation handler，你需要以相应的 type 调用 store.commit 方法
+   ***********************
+   */
+  mutations: {
+    [increment] (state, payload) {
+      state.count += payload.amount
+    },
+    decrement (state) {
+      state.count--
+    }
+  },
+  // 类似于computed
+  getters: {
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    doneTodosCount: (state, getters) => {
+      return getters.doneTodos.length
+    }
+  },
+  actions: {
+    increment (context) {
+      context.commit('increment')
+    }
+  }
+})
+
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
-  components: { App }
+  components: {App}
 })
+
+console.log(store.getters.doneTodos)
+console.log(store.getters.doneTodosCount)
